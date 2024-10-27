2e66dee068014164bc240d3b3c314d7a8cd97a01
isabellaMB19 <isabella.myersbennett@gmail.com>
2024-10-27 03:29:30 +0000

    fizzy cooking

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,11 +1,14 @@
 #================================ ZIBZIDI DNA ================================
 
 import mediapipe
+from mediapipe import solutions
 import cv2
 from collections import Counter
 import random
 from time import sleep
 import time 
+import socket
+import asyncio
 
 #draws hand framework over the hand in real time
 drawingModule = mediapipe.solutions.drawing_utils
@@ -22,6 +25,25 @@ mid = [6,10,14,18]
 fingers = []
 finger = []
 
+user_score = 0
+comp_score = 0
+
+button = True
+
+HOST = "192.168.212.251"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
+
+ret, frame = cap.read()
+frame1 = cv2.resize(frame, (640, 480))
+hands = handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7, max_num_hands=1)
+ 
 def findNameOfLandmark(frame1):
     list = []
     results = hands.process(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB))
@@ -33,36 +55,38 @@ def findNameOfLandmark(frame1):
 
 counter = 0
 
-with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, min_tracking_confidence=0.7, max_num_hands=1) as hands:
-    
-    # infinite loop that produces live feed on screen searching for hands
-    while True:
-        ret, frame = cap.read()
-        frame1 = cv2.resize(frame, (640, 480))
+def cameraLoop():
+     # infinite loop that produces live feed on screen searching for hands
+        while True:
+            asyncio.run(listenLoop())
+            ret, frame = cap.read()
+            frame1 = cv2.resize(frame, (640, 480))
 
-        list = []
-        # converts video to desired colours
-        results = hands.process(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB))
+            list = []
+            # converts video to desired colours
+            results = hands.process(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB))
 
-        # displays camera feed
-        cv2.imshow('ROCK PAPER SCISSORS', frame1)
 
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
+            # if hand is on screen
+            if results.multi_hand_landmarks != None:
+                # deals with mutliple hands
+                for handLandmarks in results.multi_hand_landmarks:
+                    list = []
+                    drawingModule.draw_landmarks(image = frame1, landmark_list = handLandmarks, connections = handsModule.HAND_CONNECTIONS)
+                    for id, pt in enumerate (handLandmarks.landmark):
+                        x = int(pt.x * w)
+                        y = int(pt.y * h)
+                        list.append([id,x,y])
 
-        # if hand is on screen
-        if results.multi_hand_landmarks != None:
-            # deals with mutliple hands
-            for handLandmarks in results.multi_hand_landmarks:
-                drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
-                list = []
-                for id, pt in enumerate (handLandmarks.landmark):
-                    x = int(pt.x * w)
-                    y = int(pt.y * h)
-                    list.append([id,x,y])
+            # displays camera feed
+            cv2.imshow('ROCK PAPER SCISSORS', frame1)
+        
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
 
-        print("list = ", list)
+async def buttonPress():
+            
         a = list
         b = findNameOfLandmark(frame1)
 
@@ -78,13 +102,76 @@ with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, mi
                     else:
                         fingers.append('down')
 
-
             print("fingers: ", fingers)
-            print("b: ", b)
             c = Counter(fingers)
             up = c['up']
             down = c['down']
+            
+            data = await s.recv(1024)
+            print(f"Received {data!r}")
+            
+
+            comp = random.choice(['rock','paper','scissors'])
+
+            if (fingers[0] == 'up' and fingers[1] == 'up' and fingers[2] == 'down' and fingers[3] == 'down'):
+                print("\nSCISSORS\n")
+
+                if (comp == "scissors"):
+                    print("COMPUTER PLAYED SCISSORS\nDRAW")
+                
+                elif (comp == "rock"):
+                
+                    print("COMPUTER PLAYED ROCK\nYOU LOSE")
+                    comp_score += 1
+                
+                else:
+                    print("COMPUTER PLAYED PAPER\nYOU WIN")
+                    user_score += 1
                 
             
+            elif (fingers[0] == 'up' and fingers[1] == 'up' and fingers[2] == 'up' and fingers[3] == 'up'):
+                print("\nPAPER\n")
+
+                if (comp == "scissors"):
+                    print("COMPUTER PLAYED SCISSORS\YOU LOSE")
+                    comp_score += 1
+                
+                elif (comp == "rock"):
+                    print("COMPUTER PLAYED ROCK\nYOU WIN")
+                    user_score += 1
+                
+                else:
+                    print("COMPUTER PLAYED PAPER\nDRAW")
+                
+            elif (fingers[0] == 'down' and fingers[1] == 'down' and fingers[2] == 'down' and fingers[3] == 'down'):
+
+                print("\nROCK")
+                if (comp == "scissors"):
+                    print("COMPUTER PLAYED SCISSORS\nYOU WIN")
+                    user_score += 1
+                
+                elif (comp == "rock"):
+                    print("COMPUTER PLAYED ROCK\nDRAW")
+                
+                else:
+                    print("COMPUTER PLAYED PAPER\nYOU LOSE")
+                    comp_score += 1
+                
+            scores = "YOU vs COMP\n",user_score,comp_score
+            s.sendall(scores)
+            #sleep(3)
+
+            
+async def listenLoop():
+    while True:
+        data = s.recv(1024)
+        print("data: ",data)
+        if data == "Button pressed":
+            buttonPress()
+
+cameraLoop()
+
+
+s.close()
 cap.release()
 cv2.destroyAllWindows()
